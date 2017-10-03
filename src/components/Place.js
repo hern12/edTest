2816b6b3e06392f93fc1ed6447893ec1c7bff08c
DESKTOP-2I4RNGJ\Kembly <hern.suwannasam@gmail.com>
2017-10-03 09:27:45 +0700

    v2

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import mapPhoto from '../images/map.jpg';
 class Place extends Component{
 
 	render(){
@@ -10,10 +10,12 @@ class Place extends Component{
 					<div className="column locate">
 						<div className="pBox">
 							59/16 ถ.จุติอนุสรณ์ อ.หาดใหญ่ จ.สงขลา <br/>เทศบาลนครหาดใหญ่ 90110
+							<br/>
+							<span>Facebook:</span><span><a href="https://www.facebook.com/endlesssoftserve/"> endlesssoftserve</a></span>
 						</div>
 					</div>
 					<div className="column map">
-						
+						<img src={mapPhoto}/>
 					</div>
 				</div>
 			</div>
